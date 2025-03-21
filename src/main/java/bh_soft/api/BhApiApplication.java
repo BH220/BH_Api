51defbd8fa980dbad7a8900511334c0d107c6d44
BH220 <61535918+BH220@users.noreply.github.com>
2025-03-21 10:14:05 +0900

    Changes

---
+++ src/main/java/bh_soft/api/BhApiApplication.java
@@ -0,0 +1,13 @@
+package bh_soft.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bh_soft/api/BhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bh_soft.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
